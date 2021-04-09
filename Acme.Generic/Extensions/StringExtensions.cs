b658a832e533457b652b362a823acf413228b0f7
benghughes <36974740+benghughes@users.noreply.github.com>
2021-04-09 08:21:31 -0500

    Add files via upload

---
+++ Acme.Generic/Extensions/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Acme.Generic.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Acme.Generic.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ConvertUnixTime(this string valueToConvert)
+        {
+            string result = "";
+
+            if (int.TryParse(valueToConvert, out int parsed))
+            {
+                result = DateTimeHelper.ConvertUnixTimeToGMTDateTime(parsed)
+                    .ToLocalTime()
+                    .ToShortTimeString();
+            }
+            return result;
+        }
+    }
+}
